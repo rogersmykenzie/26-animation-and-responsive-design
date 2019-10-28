52552c236da552678a8451f30f7929462bd0781b
mykenzie <mykenzierogers@gmail.com>
2019-10-28 12:09:11 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      menuStatus: "menu-close"
+    };
+  }
+  handleClick = e => {
+    if (this.state.menuStatus === "menu-close") {
+      this.setState({ menuStatus: "menu-open" });
+    } else {
+      this.setState({ menuStatus: "menu-close" });
+    }
+  };
+  render() {
+    return (
+      <>
+        <nav>
+          <h1>Start Bootstrap</h1>
+          <img
+            src="https://icon-library.net/images/menu-icon-white-png/menu-icon-white-png-27.jpg"
+            alt="ham_menu"
+            onClick={this.handleClick}
+          />
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <menu id="menu" className={this.state.menuStatus}></menu>
+      </>
+    );
+  }
 }
 
 export default App;
